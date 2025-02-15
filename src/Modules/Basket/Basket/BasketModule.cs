9f513dbdbdc4c34784cd44de1f282175e8a61fb5
moonlight\moonlight <nguyetanhmyangel@gmail.com>
2025-02-15 14:30:57 +0700

    test run docker for the first time

---
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Basket;
+
+public static class BasketModule
+{
+    public static IServiceCollection AddBasketModule(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        // Add services to the container.
+        //services
+        //    .AddApplicationServices()
+        //    .AddInfrastructureServices(configuration)
+        //    .AddApiServices(configuration);
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
+    {
+        // Configure the HTTP request pipeline.
+        //app
+        //    .UseApplicationServices()
+        //    .UseInfrastructureServices()
+        //    .UseApiServices();
+
+        return app;
+    }
+}
